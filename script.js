6b0e975494823de98a01c3cce3e17cd590d89212
briangonzales10 <brian.gonzales10@gmail.com>
2021-05-20 20:59:03 -0500

    updated stuff

--- script.js
+++ script.js
@@ -1,9 +1,26 @@
-const dbURL = 'https://brian-destinations.herokuapp.com/destinations/'
+
+const baseURL = 'https://brian-destinations.herokuapp.com'
 
 const destination = document.getElementById("destForm")
 destination.addEventListener("submit", handleForm)
 
 
+//Create Cards out of array elements in Database
+const dbURL = `${baseURL}/destinations`
+fetch(dbURL)
+.then((response) => response.json())
+.then((pics) =>  createCards(pics) );
+
+
+//Reach out to API and Return DB Array of destinations
+function createCards(destinations) {
+for (dest of destinations) {
+    const {id, name, location, photo, description } = dest
+
+    createCard(id, name, location, photo, description)
+
+    }
+}
 
 
 async function handleForm(evt) {
@@ -11,42 +28,63 @@ evt.preventDefault()
 const form = evt.target
 
 //Pull Form Info
-const dest = form.dest_name.value
-const loc = form.location.value
-const desc = form.desc.value
-let photo = await GrabImage(dest);
+const name = form.dest_name.value
+const location = form.location.value
+const description = form.desc.value
+// let photo = await GrabImage(dest);
 
+postDestination(name, location, description)
+// After submit, replace "Enter Destination Details" with "Wishlist" on Title
+document.getElementById("title").innerText = "Wishlist "
+resetForm(evt)
+}
+
+function postDestination(nameArg, locationArg, descriptionArg) {
+    const newDest = {
+        name: nameArg,
+        location: locationArg,
+        description: descriptionArg
+    }
+
+    const postURL = `${baseURL}/destinations`
+    fetch(postURL, {
+        method: "POST",
+        body: JSON.stringify(newDest),
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8'
+        } 
+    })
+    .then((response) => {response.json()
+        console.log('heeeelp')})
+    .then(response => { 
+        location.reload()
+        })
+        
+}
 
 //Create Functions For Card
     // Parent DIV for each card inside the "destcontainer"
-
+function createCard(id, name, location, photo, description) {
     let card = document.createElement("div");
     card.setAttribute("class","card")
     card.setAttribute("style", "width: 18rem;")
 
     card.innerHTML = `
-                    <img src="${photo}" class="card-img-top" alt="${dest}">
+                    <img src="${photo}" class="card-img-top" alt="${name}">
                     <div class="card-body">
-                      <h5 class="card-title">${dest}</h5>
-                      <h6> ${loc}</h6>
-                      <p class="card-text">${desc}</p>
-                      <a href="#" button="edit" class="btn btn-warning">Edit</a> | <a href="#" button="delete" class="btn btn-danger"> Delete </a>
+                      <h5 class="card-title">${name}</h5>
+                      <h6> ${location}</h6>
+                      <p class="card-text">${description}</p>
+                      <a href="#" uid=${id} button="edit" class="btn btn-warning">Edit</a> | <a href="#" uid=${id} button="delete" class="btn btn-danger"> Delete </a>
                     </div>
                   `;
     document.getElementById("cardcontainer").appendChild(card)  
     
     card.addEventListener("click", buttonHandler)
-
-
-// After submit, replace "Enter Destination Details" with "Wishlist" on Title
-document.getElementById("title").innerText = "Wishlist "
-resetForm(evt)
-
 }
 
 async function GrabImage(destination) {
-    //Unsplash Images
-
+//Unsplash Images
 const API = `https://api.unsplash.com/search/photos/?client_id=9xB6KzPdxq603YtE7BwTp5OsWNjOrLPRsSw_4XWKW0A&query=`
 let dest = destination;
 const searchURL = `${API}${dest}`
@@ -57,21 +95,27 @@ return fetch(searchURL)
     
 }
 
-
 function buttonHandler(evt) {
     const event = evt.target
     if (event.getAttribute("button") === "delete") {
         deleteCard(evt)
     }
     else if (event.getAttribute("button") === "edit") {
-       
         editCard(evt)
     }
 }
 
 function deleteCard(evt){
-    const event = evt.target
-    event.parentElement.parentElement.remove()
+    // const event = evt.target
+    // event.parentElement.parentElement.remove()
+    const id = evt.target.getAttribute("uid")
+    const deleteURL = `${baseURL}/destinations/${id}`
+    fetch(deleteURL,{
+        method: "DELETE"
+    })
+    .then(res => {
+    location.reload()
+    })
 }
     
 async function editCard(evt){
@@ -116,9 +160,3 @@ function resetForm(evt) {
     form.desc.value = ""
 }
 
-
-//Reach out to API and Return DB Array of destinations
-fetch(dbURL)
-    .then((response) => response.json())
-    .then((pics) => console.log(pics) );
-//Create Cards out of array elements in Database
